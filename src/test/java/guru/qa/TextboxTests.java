3090da85e324c0a570fe5ff20f25956bda599670
qwer083 <Qwertyiop890>
2022-04-16 18:21:04 +0300

    small refactor & added logger slf4j

--- src/test/java/guru/qa/TextboxTests.java
+++ src/test/java/guru/qa/TextboxTests.java
@@ -1,12 +1,43 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextboxTests {
+
+    @BeforeAll
+    static void setUp() {
+
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080 ";
+    }
+
+
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/text-box");
+
+        String name = "Alex Egorov";
+
+        open("/text-box");
+
+      $("[id=userName]").setValue(name);
+      $("[id=userEmail]").setValue("alex@egorov.com");
+      $("[id=currentAddress]").setValue("Some street 1");
+      $("[id=permanentAddress]").setValue("Another street 2");
+      $("[id=submit]").click();
+
+      $("[id=output]").shouldHave(text(name), text("alex@egorov.com"),
+              text("Some street 1"), text("Another street 2"));
+
+        $("[id=output] [id=name]").shouldHave(text(name));
+
+        $("[id=output]").$("[id=name]").shouldHave(text(name));
+
     }
 }
